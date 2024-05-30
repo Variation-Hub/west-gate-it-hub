2e3e1eb52cb48b993584ec89a6d96c93746ba6fc
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-05-30 23:48:30 +0530

    remove_token

--- src/Routes/supportRoute.ts
+++ src/Routes/supportRoute.ts
@@ -1,14 +1,14 @@
 import * as express from 'express';
 import { multipleFileUpload } from '../Util/multer';
-import { authorizeRoles } from '../Middleware/verifyToken';
+// import { authorizeRoles } from '../Middleware/verifyToken';
 import { createSupportMessage, deleteSupportMessage, getSupportMessages, getUsersChattedWithUser } from '../Controllers/supportController';
 
 const supportRoute = express.Router();
 
-supportRoute.post("/create", authorizeRoles(), multipleFileUpload("files", 5), createSupportMessage);
-supportRoute.delete("/delete/:id", authorizeRoles(), deleteSupportMessage);
-supportRoute.get("/list", authorizeRoles(), getSupportMessages);
-supportRoute.get("/user/list", authorizeRoles(), getUsersChattedWithUser);
+supportRoute.post("/create", multipleFileUpload("files", 5), createSupportMessage);
+supportRoute.delete("/delete/:id", deleteSupportMessage);
+supportRoute.get("/list", getSupportMessages);
+supportRoute.get("/user/list", getUsersChattedWithUser);
 
 
 export default supportRoute;
