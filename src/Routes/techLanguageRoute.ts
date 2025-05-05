0f0c4fd00c1e2a0d83ce78690b381fb559922b82
heeral-makwana <makwanahiral60@gmail.com>
2025-05-05 12:06:08 +0530

    feat: Add technology and language model and APIs

---
+++ src/Routes/techLanguageRoute.ts
@@ -0,0 +1,21 @@
+import * as express from 'express';
+import { 
+    createTechnology, 
+    createLanguage, 
+    deleteTechnology, 
+    deleteLanguage,
+    getTechnologies,
+    getLanguages
+} from '../Controllers/techLanguageController';
+import { authorizeRoles } from '../Controllers/Middleware/verifyToken';
+
+const techLanguageRoute = express.Router();
+
+techLanguageRoute.get("/technologies", authorizeRoles(), getTechnologies);
+techLanguageRoute.get("/languages", authorizeRoles(), getLanguages);
+techLanguageRoute.post("/technologies", authorizeRoles(), createTechnology);
+techLanguageRoute.post("/languages", authorizeRoles(), createLanguage);
+techLanguageRoute.delete("/technologies/:id", authorizeRoles(), deleteTechnology);
+techLanguageRoute.delete("/languages/:id", authorizeRoles(), deleteLanguage);
+
+export default techLanguageRoute;
